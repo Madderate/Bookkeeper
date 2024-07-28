35643ee1f2b6919bfa43d48a9a07f1287e0b7da2
Madderate <liaojianzengzhi@hotmail.com>
2024-07-28 16:22:44 +0800

    [init] Init Project Bookkeeper~

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bookkeeper"
+
+include("app")
+
+includeBuild("feature")
+includeBuild("framework")
+includeBuild("common")
